386e720dcfb4a21e8de5a3a1fdb898887c30a9ab
Jean Carlos <carlosjean875@gmail.com>
2025-01-28 22:24:23 -0300

    Adicionando funções

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+if (amigos.push(nome)){
+    console.log(nome);
+};
+
+}
